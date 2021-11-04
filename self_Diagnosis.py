6ad21330ac1299c2afbbdcddba4d6031dbe8db0e
dlatldhs <sionzz1713@gmail.com>
2021-11-04 19:21:40 +0900

    입력 받기

---
+++ self_Diagnosis.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+import pyautogui as pagx
+import pyautogui
+import time
+import time as t
+
+#변수 설정
+name=input("Your name: ")
+pwd=input("Your password: ")
+
